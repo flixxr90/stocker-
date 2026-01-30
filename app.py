95115d56d24b80dcdf0bffb961e0a94165b0e92b
vipin <vipinkumar90243@gmail.com>
2026-01-30 18:09:43 +0530

    website is ready

--- app.py
+++ app.py
@@ -1,7 +1,34 @@
 from flask import Flask, render_template
+
 app = Flask(__name__)
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/service')
+def service():
+    return render_template('service.html')
+
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')
+
+@app.route('/team')
+def team():
+    return render_template('team.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/404')
+def error_404():
+    return render_template('404.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
